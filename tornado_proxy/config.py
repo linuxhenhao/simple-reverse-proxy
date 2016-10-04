aac3c6175b5a99ba1ac51bcacd43446188bbf8a7
huangyu <diwang90@gmail.com>
2016-10-04 23:30:36 +0800

    ' ipv4.thinkeryu.com' to remove the space

--- tornado_proxy/config.py
+++ tornado_proxy/config.py
@@ -26,7 +26,7 @@ scihub_self = 'scihub.thinkeryu.com'
 
 rules_source = [
 ('https://scholar.google.com', 'scholar.thinkeryu.com'),
-('https://ipv4.google.com',' ipv4.thinkeryu.com'),
+('https://ipv4.google.com','ipv4.thinkeryu.com'),
 ('http://sci-hub.bz', scihub_self)
 ]
 
