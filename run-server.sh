f443be8c8a29110e7b8f0dbccb9f551ad17fa7c9
huangyu <diwang90@gmail.com>
2016-10-04 19:18:50 +0800

    remove runnig container before docker rmi by adding container name

--- run-server.sh
+++ run-server.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+docker rm -f scihub_container
 docker rmi -f scihub
+
 cd /root/tornado_proxy && git pull
 cp /root/tornado_proxy/Dockerfile /root/docker/
 
 cd /root
 docker build -t scihub ./docker
 
-docker run -v /etc/letsencrypt/archive/scholar.thinkeryu.com/:/media/ -p 80:80 -p 443:443 scihub
+docker run --name scihub_container -v /etc/letsencrypt/archive/scholar.thinkeryu.com/:/media/ -p 80:80 -p 443:443 scihub
