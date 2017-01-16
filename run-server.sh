f7aa6a720216692c1534b60224fc8a1f4cd36e8d
huangyu <diwang90@gmail.com>
2017-01-16 23:45:22 +0800

    modified run-server.sh to run dns server on host side

--- run-server.sh
+++ run-server.sh
@@ -3,6 +3,9 @@ docker rm -f scihub_container
 docker rmi -f scihub
 
 cd /root/tornado_proxy && git pull
+pip install -r /root/tornado_proxy/requirements.txt
+python /root/tornado_proxy/tornado_proxy/findmegoogleip.py &
+
 cp /root/tornado_proxy/Dockerfile /root/docker/
 
 cd /root
