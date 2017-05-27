c307725d09ac15e8f36d3fc55d643ab5b6c151c3
huangyu <diwang90@gmail.com>
2017-05-27 12:37:10 +0800

    allow ipv6 = true

--- tornado_proxy/proxy.py
+++ tornado_proxy/proxy.py
@@ -368,7 +368,7 @@ class ProxyHandler(tornado.web.RequestHandler):
                 self.request.uri, handle_response,
                 method=self.request.method, body=body,
                 headers=headers, follow_redirects=False,
-                allow_nonstandard_methods=True, allow_ipv6=False)
+                allow_nonstandard_methods=True, allow_ipv6=True)
         except tornado.httpclient.HTTPError as e:
             if hasattr(e, 'response') and e.response:
                 handle_response(e.response)
